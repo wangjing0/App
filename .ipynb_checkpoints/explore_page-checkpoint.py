4c65a103db56d2c73787dd9ba91feb7718ad8345
Jing Wang <jingwang.physics@gmail.com>
2022-07-15 20:45:36 -0500

    fix

--- .ipynb_checkpoints/explore_page-checkpoint.py
+++ .ipynb_checkpoints/explore_page-checkpoint.py
@@ -16,23 +16,39 @@ df = load_data()
 
 def show_explore_page():
     width = 800
-    st.title("Explore Activities")
+    st.title("Activities, data and insights")
 
     st.write(
     """
-    ### Geographic distribution of our users
+    ###Geographic distribution of our users over the world
     """
     )
     
     image = Image.open('./images/geo_density_.png')
-    st.image(image, caption="Users Over the world ",use_column_width=True)
+    #st.image(image, caption="Geographic distribution of our users over the world ",use_column_width=True)
     
+    st.write(
+    """
+    ###Heart rate history of individual user
+    """
+    )
     image = Image.open('./images/hr_history.png')
-    st.image(image, caption="Heart rate history of individual user.",use_column_width=True)
+    #st.image(image, caption="Heart rate history of individual user.",use_column_width=True)
     
+    st.write(
+    """
+    ###Training Route example
+    """
+    )
     image = Image.open('./images/route_run.png')
-    st.image(image, caption="Training Route",use_column_width=True)
+    #st.image(image, caption="Training Route",use_column_width=True)
     
+    
+    st.write(
+    """
+    ###Heart rate and Altitude
+    """
+    )
     image = Image.open('./images/hr_session_detail.png')
-    st.image(image, caption="Heart and altitude",use_column_width=True)
+    #st.image(image, caption="Heart and altitude",use_column_width=True)
     
